26186f02af5b85c54e4287350ed8c2896a06d899
川島慎悟 <kawashingo1990@gmail.com>
2022-08-12 16:20:01 +0900

    力試し -2-

--- src/section4/index.ts
+++ src/section4/index.ts
@@ -1,5 +1,6 @@
 export {}
 
+// 力試し1
 const getFizzBuzzString = (
   i: number
 ): number | ' FizzBuzz' | ' Buzz' | ' Fizz' => {
@@ -26,3 +27,20 @@ for (const i of sequence(1, 100)) {
   const message = getFizzBuzzString(i)
   console.log(message)
 }
+
+// 力試し2
+function map<T, U>(array: T[], callback: (value: T) => U): U[] {
+  const result: U[] = []
+  for (const elm of array) {
+    result.push(callback(elm))
+  }
+  return result
+}
+
+const data = [1, 1, 2, 3, 5, 8, 13]
+const result = map(data, (x) => x * 10)
+console.log(result)
+
+const data2 = [1, -3, -2, 8, 0, -1]
+const result2 = map(data2, (x) => x >= 0)
+console.log(result2)
