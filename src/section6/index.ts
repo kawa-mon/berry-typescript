1f997debb3a1d6f7958f9605014a5c35b01229a6
川島慎悟 <kawashingo1990@gmail.com>
2022-08-21 15:01:56 +0900

    力試し -1-

---
+++ src/section6/index.ts
@@ -0,0 +1,29 @@
+export {}
+
+// 力試し1
+type Option<T> =
+  | {
+      tag: 'some'
+      value: T
+    }
+  | {
+      tag: 'none'
+    }
+
+function showNumberIfExist(obj: Option<number>): void {
+  if (obj.tag === 'some') {
+    console.log(obj.value)
+  }
+}
+
+const four: Option<number> = {
+  tag: 'some',
+  value: 4,
+}
+
+const nothing: Option<number> = {
+  tag: 'none',
+}
+
+showNumberIfExist(four)
+showNumberIfExist(nothing)
