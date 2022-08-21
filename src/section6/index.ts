7a5a21abc7f7b5c595a7ec9828d8b52ef5fc4cbf
川島慎悟 <kawashingo1990@gmail.com>
2022-08-21 15:17:40 +0900

    力試し -3-

--- src/section6/index.ts
+++ src/section6/index.ts
@@ -26,3 +26,20 @@ const nothing: Option<number> = {
 
 showNumberIfExist(four)
 showNumberIfExist(nothing)
+
+// 力試し3
+function doubleOption(obj: Option<number>) {
+  return mapOption(obj, (x) => x * 2)
+}
+
+function mapOption<T, U>(obj: Option<T>, callback: (value: T) => U): Option<U> {
+  switch (obj.tag) {
+    case 'some':
+      return { tag: 'some', value: callback(obj.value) }
+    case 'none':
+      return { tag: 'none' }
+  }
+}
+
+console.log(doubleOption(four))
+console.log(doubleOption(nothing))
